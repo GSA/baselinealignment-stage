85edc1e5c70b2ae02e1720caacf74b1b508f48ba
HarikaMunipalle <111448174+HarikaMunipalle@users.noreply.github.com>
2024-07-25 16:30:51 -0400

    Left Popup

--- assets/js/baseline-align.js
+++ assets/js/baseline-align.js
@@ -1,13 +1,13 @@
 // Toggle between showing and hiding the sidebar, and add overlay effect
 function sidebarOpenClose() {
-	$("#site-nav").toggleClass("flyover-sidebar");
-	$("#site-nav").toggleClass("d-none");
-	$("#site-nav").toggleClass("col");
-	$("#site-nav").toggleClass("d-md-block");
-	$("#site-nav").toggleClass("col-md-3");
-	$("#site-nav").toggleClass("col-xl-2");
-	$("#site-nav").toggleClass("fixed-sidebar");
-	$("#site-nav").toggleClass("order-0");
+	$("#site-navBaseline").toggleClass("flyover-sidebar");
+	$("#site-navBaseline").toggleClass("d-none");
+	$("#site-navBaseline").toggleClass("col");
+	$("#site-navBaseline").toggleClass("d-md-block");
+	$("#site-navBaseline").toggleClass("col-md-3");
+	$("#site-navBaseline").toggleClass("col-xl-2");
+	$("#site-navBaseline").toggleClass("fixed-sidebar");
+	$("#site-navBaseline").toggleClass("order-0");
 	$("#closeSidebar").toggleClass("d-none");
 	$("#myOverlay").toggleClass("d-none");
 	var openCloseMenu = document.getElementById("openCloseMenu")
